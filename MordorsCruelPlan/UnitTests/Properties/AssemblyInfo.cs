2195871a934ca7c8359c54b65aa875fce56bd5e3
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-19 18:18:49 +0100

    initial

---
+++ MordorsCruelPlan/UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6698e27d-a898-48fb-a6bf-456089f8e8a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MordorsCruelPlan/UnitTests/StringManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{   
+    [TestClass]
+    public class StringManagerTests
+    {
+        [TestMethod]
+        public void IchGuckeObDerManagerStringsTeilt()
+        {
+            //Arrange
+            var foodStringInput = "Banane Apfel Huhn";
+            var outputExpected1 = "Banane";
+            var outputExpected2 = "Apfel";
+            var outputExpected3 = ""
+
+            //Act
+            //Assert
+        }
+    }
+}
